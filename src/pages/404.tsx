2d1dd11c94db5d622a3ccf64213526c219baeb13
watasuke102 <watasuke102@gmail.com>
2024-10-19 20:58:53 +0900

    update: 404>more info and link to /

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,12 +5,17 @@
 // Twitter: @Watasuke102
 // This software is released under the MIT or MIT SUSHI-WARE License.
 import React from 'react';
+import {Link} from 'gatsby';
+import {Header} from '../common/Header';
 
-export default function Index(): React.ReactElement {
+export default function NotFound(): React.ReactElement {
   return (
     <>
-      <main>
+      <Header />
+      <main style={{paddingTop: 80, textAlign: 'center'}}>
         <h1>404</h1>
+        <p>ページが見つかりませんでした。</p>
+        <Link to='/'>トップページに戻る</Link>
       </main>
     </>
   );
